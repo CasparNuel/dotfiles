e9b9758b8e0744779d9feb64cc6635f0bb219e35
Caspar Nuël <xbmbmx>
2024-02-08 19:50:53 +0100

    conf(nvim): add toggleterm open keybind, undotree keybind

--- .config/nvim/after/plugin/toggleterm.lua
+++ .config/nvim/after/plugin/toggleterm.lua
@@ -1,2 +1,13 @@
 require("toggleterm").setup({
+	open_mapping = [[<Space>t]],
+	direction = "float",
+	float_opts = {
+		border = "curved"
+	},
+	winbar = {
+		enabled = true,
+		name_formatter = function (term)
+			return term.name
+		end
+	}
 })
