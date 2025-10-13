ab97ccfbabe1954b9614157e88b77ad7633049d3
Caspar Nuël <casparnuel@yandex.com>
2025-10-13 17:02:12 +0200

    fix: keybind issues with luasnip resolved through nvim-cmp

--- .config/nvim/after/plugin/luasnip.lua
+++
@@ -1,5 +0,0 @@
-local ls = require("luasnip")
-
-vim.keymap.set({"i"}, "<C-K>", function() ls.expand() end, {silent = true})
-vim.keymap.set({"i", "s"}, "<S-Tab>", function() ls.jump(1) end, {silent = true})
--- vim.keymap.set({"i", "s"}, "<S-Tab>", function() ls.jump(-1) end, {silent = true}) -- going back in fields is currently not done, conflicts with Tab in insert mode otherwise
