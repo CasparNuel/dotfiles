95e52114fb6b2c8e6093f111c0cead3b54518771
Caspar Nuël <casparnuel@yandex.com>
2025-10-22 20:22:34 +0200

    feat: always show signcolumn
    This solves the screens shifting unexpectedly for language servers that are slow to process the current file.

--- .config/nvim/lua/caspar/init.lua
+++ .config/nvim/lua/caspar/init.lua
@@ -2,6 +2,7 @@ vim.opt.relativenumber = true
 vim.opt.hlsearch = false
 vim.opt.number = true
 vim.opt.scrolloff = 999
+vim.opt.signcolumn = "yes"
 
 require("caspar.remap")
 require("caspar.filetype")
