76a10237c35c67c5099cf3627f004e89efc23599
Caspar Nuël <casparnuel@yandex.com>
2025-11-24 17:44:02 +0100

    feat(nvim): nvim-tree auto-focus file in active buffer

--- .config/nvim/after/plugin/nvim-tree.lua
+++ .config/nvim/after/plugin/nvim-tree.lua
@@ -5,3 +5,15 @@ require("nvim-tree").setup({
 -- Remaps:
 vim.keymap.set("n", "<C-f>", vim.cmd.NvimTreeToggle)
 vim.keymap.set("n", "F", vim.cmd.NvimTreeFocus)
+
+-- Highlight files when navigating buffers
+local api = require("nvim-tree.api")
+
+vim.api.nvim_create_autocmd("BufEnter", {
+    nested = true,
+    callback = function()
+        if (vim.fn.bufname() == "NvimTree_1") then return end
+
+        api.tree.find_file({ buf = vim.fn.bufnr() })
+    end,
+})
