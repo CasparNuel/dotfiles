2c7dc1dacc53b3a163d711acf5900d2357af43a3
Caspar Nuël <casparnuel@yandex.com>
2025-11-25 09:10:45 +0100

    fix(nvim): folding behaviour

--- .config/nvim/lua/caspar/init.lua
+++ .config/nvim/lua/caspar/init.lua
@@ -5,11 +5,22 @@ vim.opt.scrolloff = 999
 vim.opt.signcolumn = "yes"
 
 vim.opt.foldmethod = "expr"
-vim.opt.foldexpr = "v:lua.vim.lsp.foldexpr()"
-vim.opt.foldlevel = 99
+vim.opt.foldexpr = "v:lua.vim.treesitter.foldexpr()"
 vim.opt.foldtext = ""
+vim.opt.foldlevel = 0
 vim.opt.foldlevelstart = 0
 vim.opt.foldnestmax = 1
+
+vim.api.nvim_set_keymap('i', 'jk', '<ESC>zv', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('i', '<ESC>', '<ESC>zv', { noremap = true, silent = true })
+
+-- fix for erratic behaviour editing inside fold
+vim.cmd([[
+	augroup folds
+	autocmd InsertEnter * if !exists('w:last_fdm') | let w:last_fdm=&foldmethod | setlocal foldmethod=manual | endif
+	autocmd InsertLeave,WinLeave * if exists('w:last_fdm') | let &l:foldmethod=w:last_fdm | unlet w:last_fdm | endif
+	augroup END
+]])
 
 require("caspar.remap")
 require("caspar.filetype")
