708c25176ac75517d8d923dca735562ef0624cd3
Caspar Nuël <casparnuel@yandex.com>
2026-09-23 15:15:19 +0200

    feat: configuration for cmake-tools

--- .config/nvim/after/plugin/cmake-tools.lua
+++ .config/nvim/after/plugin/cmake-tools.lua
@@ -1,4 +1,8 @@
 local osys = require("cmake-tools.osys")
 require("cmake-tools").setup({
-	cmake_kits_path = vim.env.CMAKE_KITS_PATH
+	cmake_kits_path = vim.env.CMAKE_KITS_PATH,
+	cmake_compile_commands_options = {
+		action = "lsp"
+	},
+	cmake_build_directory = "build"
 })
